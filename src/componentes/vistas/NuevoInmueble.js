731a337a007c73d50583950f28fe8142a086ae68
migue <miguelexd@hotmail.com>
2020-03-27 09:20:09 +0100

    Alamcenamiento local

--- src/componentes/vistas/NuevoInmueble.js
+++ src/componentes/vistas/NuevoInmueble.js
@@ -1,161 +1,220 @@
-import React, { Component } from 'react';
-import { Container, Paper, Grid, Breadcrumbs, Link, Typography, TextField, Button } from '@material-ui/core';
-import HomeIcon from '@material-ui/icons/Home';
-import {consumerFirebase} from '../../server'
-import { openMensajePantalla} from '../../sesion/actions/snackbarAction';
+import React, { Component } from "react";
+import {
+  Container,
+  Paper,
+  Grid,
+  Breadcrumbs,
+  Link,
+  Typography,
+  TextField,
+  Button,
+  Table,
+  TableBody,
+  TableRow,
+  TableCell
+} from "@material-ui/core";
+import HomeIcon from "@material-ui/icons/Home";
+import { consumerFirebase } from "../../server";
+import { openMensajePantalla } from "../../sesion/actions/snackbarAction";
+import ImageUploader from "react-images-upload";
 
 const style = {
-    container : {
-        paddingTop : '8px'
-    },
+  container: {
+    paddingTop: "8px"
+  },
 
-    paper : {
-        marginTop : 8,
-        display :"flex",
-        flexDirection : 'column',
-        alignItem : 'center',
-        padding : '20px',
-        backgroundColor : '#f5f5f5'
-    },
+  paper: {
+    marginTop: 8,
+    display: "flex",
+    flexDirection: "column",
+    alignItem: "center",
+    padding: "20px",
+    backgroundColor: "#f5f5f5"
+  },
 
-    link : {
-        display : 'flex'
-    },
-    
-    homeIcon : {
-        width : 20,
-        height  : 20,
-        marginRight : '4px' 
-    },
+  link: {
+    display: "flex"
+  },
 
-    submit : {
-        marginTop : 15,
-        marginBottom : 10
-    }
-}
+  homeIcon: {
+    width: 20,
+    height: 20,
+    marginRight: "4px"
+  },
+
+  submit: {
+    marginTop: 15,
+    marginBottom: 10
+  },
+  foto: {
+    height: "100px"
+  }
+};
 
 class NuevoInmueble extends Component {
+  state = {
+    inmueble: {
+      direccion: "",
+      ciudad: "",
+      pais: "",
+      descripcion: "",
+      interior: "",
+      fotos: []
+    },
 
-    state = {
-        inmueble : {
-            direccion : '',
-            ciudad : '',
-            pais: '',
-            descripcion : '',
-            interior : ''
-        }
-    }
+    archivos: []
+  };
 
-    entraDatoEnEstado = e => {
-        let inmueble_ = Object.assign({}, this.state.inmueble);
-        inmueble_[e.target.name] = e.target.value;
-        this.setState({
-            inmueble : inmueble_
-        })
-    }
+  entraDatoEnEstado = e => {
+    let inmueble_ = Object.assign({}, this.state.inmueble);
+    inmueble_[e.target.name] = e.target.value;
+    this.setState({
+      inmueble: inmueble_
+    });
+  };
+  subirFotos = documentos => {
+    Object.keys(documentos).forEach(function(key) {
+      documentos[key].urlTemp = URL.createObjectURL(documentos[key]);
+    });
 
-    guardarInmueble = () => {
-        const {inmueble} = this.state;
+    this.setState({
+      archivos: this.state.archivos.concat(documentos)
+    });
+  };
 
-        this.props.firebase.db
-        .collection("Inmuebles")
-        .add(inmueble)
-        .then(success => {
-            this.props.history("/")
-        })
-        .catch(error => {
-            openMensajePantalla({
-                open : true,
-                mensaeje : error
-            })
-        })
+  guardarInmueble = () => {
+    const { inmueble } = this.state;
 
-    }
+    this.props.firebase.db
+      .collection("Inmuebles")
+      .add(inmueble)
+      .then(success => {
+        this.props.history("/");
+      })
+      .catch(error => {
+        openMensajePantalla({
+          open: true,
+          mensaeje: error
+        });
+      });
+  };
 
-    render() {
-        return (
-            <Container style = {style.container}>
-                <Paper style = {style.paper}>
-                    <Grid container spacing ={3}>
-                        <Grid item xs={12} md={8}>
-                            <Breadcrumbs aria-label ="breadcrumb">
-                                <Link color="inherit" style={style.link} href="/">
-                                    <HomeIcon style={style.homeIcon} />
-                                    Home
-                                </Link>
-                                <Typography color="textPrimary">Nuevo Inmueble</Typography>
-                            </Breadcrumbs>
-                        </Grid>
-                        <Grid item xs={12} md={12}>
-                            <TextField
-                                name="direccion"
-                                label="Direccion del inmueble"
-                                fullWidth
-                                onChange = {this.entraDatoEnEstado}
-                                value={this.state.inmueble.direccion}
-                            />
-                        </Grid>
-                        <Grid item xs={12} md={6}>
-                            <TextField
-                                name="ciudad"
-                                label="Ciudad"
-                                fullWidth
-                                onChange = {this.entraDatoEnEstado}
-                                value={this.state.inmueble.ciudad}
-                            />
-                        </Grid>
-                        <Grid item xs={12} md={6}>
-                            <TextField
-                                name="pais"
-                                label="País"
-                                fullWidth
-                                onChange = {this.entraDatoEnEstado}
-                                value={this.state.inmueble.pais}
-                            />
-                        </Grid>
-                        <Grid item xs={12} md={12}>
-                            <TextField
-                                name="descripcion"
-                                label="Descripcion del inmueble"
-                                fullWidth
-                                multiline
-                                onChange = {this.entraDatoEnEstado}
-                                value={this.state.inmueble.descripcion}
-                            />
-                        </Grid>
-                        <Grid item xs={12} md={12}>
-                            <TextField
-                                name="interior"
-                                label="Interior del inmueble"
-                                fullWidth
-                                multiline
-                                onChange = {this.entraDatoEnEstado}
-                                value={this.state.inmueble.interior}
-                            />
-                        </Grid>
-                    </Grid>
+  render() {
+    return (
+      <Container style={style.container}>
+        <Paper style={style.paper}>
+          <Grid container spacing={3}>
+            <Grid item xs={12} md={8}>
+              <Breadcrumbs aria-label="breadcrumb">
+                <Link color="inherit" style={style.link} href="/">
+                  <HomeIcon style={style.homeIcon} />
+                  Home
+                </Link>
+                <Typography color="textPrimary">Nuevo Inmueble</Typography>
+              </Breadcrumbs>
+            </Grid>
+            <Grid item xs={12} md={12}>
+              <TextField
+                name="direccion"
+                label="Direccion del inmueble"
+                fullWidth
+                onChange={this.entraDatoEnEstado}
+                value={this.state.inmueble.direccion}
+              />
+            </Grid>
+            <Grid item xs={12} md={6}>
+              <TextField
+                name="ciudad"
+                label="Ciudad"
+                fullWidth
+                onChange={this.entraDatoEnEstado}
+                value={this.state.inmueble.ciudad}
+              />
+            </Grid>
+            <Grid item xs={12} md={6}>
+              <TextField
+                name="pais"
+                label="País"
+                fullWidth
+                onChange={this.entraDatoEnEstado}
+                value={this.state.inmueble.pais}
+              />
+            </Grid>
+            <Grid item xs={12} md={12}>
+              <TextField
+                name="descripcion"
+                label="Descripcion del inmueble"
+                fullWidth
+                multiline
+                onChange={this.entraDatoEnEstado}
+                value={this.state.inmueble.descripcion}
+              />
+            </Grid>
+            <Grid item xs={12} md={12}>
+              <TextField
+                name="interior"
+                label="Interior del inmueble"
+                fullWidth
+                multiline
+                onChange={this.entraDatoEnEstado}
+                value={this.state.inmueble.interior}
+              />
+            </Grid>
+          </Grid>
 
-                    <Grid container justify="center">
-                        <Grid item xs={12} md={6}>
-                            <Button
-                                type="buttoon"
-                                fullWidth
-                                variant="contained"
-                                size="large"
-                                color="primary"
-                                style={style.submit}
-                                onClick={this.guardarInmueble}
-                            >
-                                Guardar
-                            </Button>
-                        </Grid>
-                    </Grid>
-                    
-                </Paper>
-
-            </Container>
-        );
-    }
+          <Grid container justify="center">
+            <Grid item xs={12} sm={6}>
+              <ImageUploader
+                key={1000}
+                withIcon={true}
+                buttonText="Seleccione imagenes"
+                onChange={this.subirFotos}
+                imgExtension={[".jpg", ".gif", ".png", ".jpeg"]}
+                maxFileSize={524288}
+              />
+            </Grid>
+            <Grid item xs={12} sm={6}>
+              <Table>
+                <TableBody>
+                  {this.state.archivos.map((archivo, i) => {
+                    <TableRow key={i}>
+                      <TableCell align="left">
+                        <img src={archivo.urlTemp} style={style.foto} />
+                      </TableCell>
+                      <TableCell>
+                        <Button
+                          variant="contained"
+                          color="secondary"
+                          size="small"
+                        >
+                          Eliminar
+                        </Button>
+                      </TableCell>
+                    </TableRow>;
+                  })}
+                </TableBody>
+              </Table>
+            </Grid>
+          </Grid>
+          <Grid container justify="center">
+            <Grid item xs={12} md={6}>
+              <Button
+                type="buttoon"
+                fullWidth
+                variant="contained"
+                size="large"
+                color="primary"
+                style={style.submit}
+                onClick={this.guardarInmueble}
+              >
+                Guardar
+              </Button>
+            </Grid>
+          </Grid>
+        </Paper>
+      </Container>
+    )
+  }
 }
 
 export default consumerFirebase(NuevoInmueble);
--- src/server/firebase.js
+++ src/server/firebase.js
@@ -21,6 +21,16 @@ class Firebase {
         this.db = app.firestore();
         this.auth = app.auth();
         this.storage = app.storage();
+
+        this.storage.ref().constructor.prototype.guardarDocumentos = function (documentos) {
+            var ref = this;
+
+            return Promise.all(documentos.map(function(file){
+                return ref.child(file.alias).put(file).then(snapshot => {
+                    return ref.child(file.alias).getDownloadURL();
+                })
+            }))
+        }
     }
 
     estaIniciado() {
@@ -32,6 +42,8 @@ class Firebase {
     guardarDocumento = (nombreDocumento, documento)=> this.storage.ref().child(nombreDocumento).put(documento);
 
     devolverDocumento = (documentoUrl) => this.storage.ref().child(documentoUrl).getDownloadURL();
+
+    guardarDocumentos = (documentos) => this.storage.ref().guardarDocumentos(documentos);
 }
 
 export default Firebase;
